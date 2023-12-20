c922c51c7a1a96d51964e9a66e8154ae46a0d604
Kirill Dubovitskiy <kirill2003de@gmail.com>
2023-12-19 23:23:14 -0800

    Initial Commit

---
+++ RightClick/RightClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightClickApp.swift
+//  RightClick
+//
+//  Created by Kirill Dubovitskiy on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RightClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
